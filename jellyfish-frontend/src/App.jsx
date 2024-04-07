8bf9f492df64a344917350628afc1e9870e1f100
Judy <judychue@pursuit.org>
2024-04-06 22:02:47 -0400

    added 404 page

--- jellyfish-frontend/src/App.jsx
+++ jellyfish-frontend/src/App.jsx
@@ -1,3 +1,50 @@
+// import { useState } from 'react'
+// import React from 'react';
+// import './App.css'
+// import './index.css';
+
+// DEPENDENCIES
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// PAGES
+// import Header from "./components/Header";
+// import NavBar from "./components/NavBar";
+// import Footer from "./components/Footer";
+// import Index from "./pages/Index";
+// import Home from "./pages/Home";
+// import Edit from "./pages/Edit";
+// import Show from "./pages/Show";
+// import New from "./pages/New";
+// import FourOFour from "./pages/FourOFour";
+
+// function App() {
+//   return (
+//     <div className="Jellyfish">
+//       <Router>
+//       <NavBar />
+//       <Header/>
+//         <main>
+//           <Routes>
+//             <Route path="/" element={<Home />} />
+//             <Route path="/jellyfish" element={<Index />} />
+//             <Route path="/jellyfish/new" element={<New />} />
+//             <Route path="/jellyfish/:id" element={<Show />} />
+//             <Route path="/jellyfish/:id/edit" element={<Edit />} />
+//             <Route path="*" element={<FourOFour />} />
+//             <Route path="/jellyfish/*" element={<FourOFour />} />
+//           </Routes>
+//           </main>
+//         <Footer/>
+//       </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
 import { useState } from 'react'
 import React from 'react';
 import './App.css'
@@ -21,8 +68,8 @@ function App() {
   return (
     <div className="Jellyfish">
       <Router>
-      <NavBar />
-      <Header/>
+        <NavBar />
+        <Header/>
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
@@ -31,9 +78,8 @@ function App() {
             <Route path="/jellyfish/:id" element={<Show />} />
             <Route path="/jellyfish/:id/edit" element={<Edit />} />
             <Route path="*" element={<FourOFour />} />
-            <Route path="/jellyfish/*" element={<FourOFour />} />
           </Routes>
-          </main>
+        </main>
         <Footer/>
       </Router>
     </div>
--- jellyfish-frontend/src/pages/FourOFour.jsx
+++ jellyfish-frontend/src/pages/FourOFour.jsx
@@ -1,12 +1,14 @@
 // <!-- this refers to the no image pose not a yoga pose -->
 
-import noImage from '../components/image/no-Image.jpg';
+import './FourOFour.css';
+import noImage from '../components/image/no-Image.png';
 
 function FourOFour() {
     return (
       <div className="not-found">
+        <h1 className="text-not-found">Sorry, Page not found!</h1>
+        {/* <h1>Sorry, Page not found!</h1> */}
         <img src={noImage}alt="jellyfish"/>
-        <h1>Sorry, Page not found!</h1>
       </div>
     );
   }
